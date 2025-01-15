e2032fa998a288735291733486f9279fc6c3c2e5
AlanTuecci <Alan.tuecci29@myhunter.cuny.edu>
2025-01-15 17:12:26 -0500

    Version 1.4.0
    Now using /gallery/api instead of /api/
    Properly uses environment variables in the backend
    Removed unnecessary dependencies

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,11 @@
-const express = require('express');
-const cors = require('cors');
-const path = require('path');
-const fs = require('fs/promises');
-const axios = require('axios');
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+const fs = require("fs/promises");
+const axios = require("axios");
+
+const { config } = require("dotenv");
+config();
 
 const app = express();
 const URL = process.env.REACT_APP_URL || "gallery-3vk6.onrender.com";
@@ -10,27 +13,27 @@ const PORT = process.env.REACT_APP_PORT || 3000;
 
 app.use(cors());
 
-app.use(express.static("../frontend/build"))
+app.use(express.static("../frontend/build"));
 
 //Defines the function used to give the client access to the .json file for the photos requested
 //The .json files are written by Alan Tuecci and contain all image information such as camera information, time and data, and a link to it (the images themselves are stored in Cloudinary)
-app.get('/api/images/:gallery', async (req, res) => {
+app.get("/gallery/api/images/:gallery", async (req, res) => {
   let galleryName = req.params.gallery;
   galleryName = path.basename(galleryName);
-  const jsonFilePath = path.join(__dirname, 'photo-metadata', `${galleryName}.json`);
+  const jsonFilePath = path.join(__dirname, "photo-metadata", `${galleryName}.json`);
 
   try {
-    const jsonData = await fs.readFile(jsonFilePath, 'utf-8');
+    const jsonData = await fs.readFile(jsonFilePath, "utf-8");
     const imageArray = JSON.parse(jsonData);
     res.json(imageArray);
   } catch (error) {
-    console.error('Error reading JSON file:', error);
-    res.status(500).json({ error: 'Internal Server Error' });
+    console.error("Error reading JSON file:", error);
+    res.status(500).json({ error: "Internal Server Error" });
   }
 });
 
 //Defines the function used to communicate and retrieve data from the OpenMeteo Historical Weather API
-app.get('/api/weather/:latitude/:longitude/:date', async (req, res) => {
+app.get("/gallery/api/weather/:latitude/:longitude/:date", async (req, res) => {
   const { latitude, longitude, date } = req.params;
 
   try {
@@ -56,13 +59,13 @@ app.get('/api/weather/:latitude/:longitude/:date', async (req, res) => {
 
     res.json(weatherData);
   } catch (error) {
-    console.error('Error fetching weather data:', error);
-    res.status(500).json({ error: 'Internal Server Error' });
+    console.error("Error fetching weather data:", error);
+    res.status(500).json({ error: "Internal Server Error" });
   }
 });
 
 //Defines the function used to communicate and retrieve data from the OpenMeteo Weather Forecast API
-app.get('/api/currentweather/:latitude/:longitude', async (req, res) => {
+app.get("/gallery/api/currentweather/:latitude/:longitude", async (req, res) => {
   const { latitude, longitude } = req.params;
 
   try {
@@ -88,12 +91,20 @@ app.get('/api/currentweather/:latitude/:longitude', async (req, res) => {
 
     res.json(currentWeatherData);
   } catch (error) {
-    console.error('Error fetching current weather data:', error);
-    res.status(500).json({ error: 'Internal Server Error' });
+    console.error("Error fetching current weather data:", error);
+    res.status(500).json({ error: "Internal Server Error" });
   }
 });
 
+//fixes the "cannot GET /url" error when refreshing the client-side app.
+//ensures that all server requests will be redirected to index.html, which
+//will make sure that the react-router-dom will handle the appropriate requests.
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend/build", "index.html"));
+});
+
 app.listen(PORT, () => {
+  console.log(`Server is running in: ${process.env.NODE_ENV} mode.`);
   console.log(`Server is running on url: ${URL}.`);
   console.log(`Server is running on port: ${PORT}.`);
 });
--- frontend/src/components/ImageGallery.js
+++ frontend/src/components/ImageGallery.js
@@ -1,16 +1,16 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import { useParams } from 'react-router-dom';
-import Navbar from './Navbar';
-import './ImageGallery.css';
-import '../App.css';
-import ImageDetails from './ImageDetails';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { useParams } from "react-router-dom";
+import Navbar from "./Navbar";
+import "./ImageGallery.css";
+import "../App.css";
+import ImageDetails from "./ImageDetails";
 
-const URL = process.env.REACT_APP || "gallery-3vk6.onrender.com";
+const URL = process.env.REACT_APP_URL || "gallery-3vk6.onrender.com";
 
 const ImageGallery = () => {
   const { id } = useParams();
-  console.log('Received id:', id);
+  console.log("Received id:", id);
 
   const [images, setImages] = useState([]);
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
@@ -22,25 +22,29 @@ const ImageGallery = () => {
     const fetchData = async () => {
       try {
         //Calls server API to get the data related to the images taken at a certain location
-        const response = await axios.get(`https://${URL}/api/images/${id}`);
-        console.log('Server Response:', response.data);
+        const response = await axios.get(`https://${URL}/gallery/api/images/${id}`);
+        console.log("Server Response:", response.data);
         setImages(response.data.resources);
 
         if (response.data.resources.length > 0) {
           const { latitude, longitude, date } = response.data.resources[0];
 
-          //Calls server API to get the Weather Data at the location where the photo was taken at the day of when the photo was taken 
-          const pastWeatherResponse = await axios.get(`https://${URL}/api/weather/${latitude}/${longitude}/${date}`);
-          console.log('Past Weather Data:', pastWeatherResponse.data);
+          //Calls server API to get the Weather Data at the location where the photo was taken at the day of when the photo was taken
+          const pastWeatherResponse = await axios.get(
+            `https://${URL}/gallery/api/weather/${latitude}/${longitude}/${date}`
+          );
+          console.log("Past Weather Data:", pastWeatherResponse.data);
           setWeather(pastWeatherResponse.data);
 
           //Calls server API to get the Current Weather Data at the location where the photo was taken at the current day
-          const currentWeatherResponse = await axios.get(`https://${URL}/api/currentweather/${latitude}/${longitude}`);
-          console.log('Current Weather Data:', currentWeatherResponse.data);
+          const currentWeatherResponse = await axios.get(
+            `https://${URL}/gallery/api/currentweather/${latitude}/${longitude}`
+          );
+          console.log("Current Weather Data:", currentWeatherResponse.data);
           setCurrentWeather(currentWeatherResponse.data);
         }
       } catch (error) {
-        console.error('Error fetching data:', error);
+        console.error("Error fetching data:", error);
       }
     };
 
@@ -63,7 +67,7 @@ const ImageGallery = () => {
     <div>
       <Navbar />
       <div className="container">
-        <div className='gallery-container'>
+        <div className="gallery-container">
           <div className="gallery">
             {images.length > 0 && (
               <div className="image-item">
@@ -72,8 +76,12 @@ const ImageGallery = () => {
             )}
             {images.length > 1 && (
               <div className="navigation-buttons">
-                <button className="nav-button" onClick={handlePrevClick}>Previous</button>
-                <button className="nav-button" onClick={handleNextClick}>Next</button>
+                <button className="nav-button" onClick={handlePrevClick}>
+                  Previous
+                </button>
+                <button className="nav-button" onClick={handleNextClick}>
+                  Next
+                </button>
               </div>
             )}
           </div>
@@ -88,7 +96,7 @@ const ImageGallery = () => {
               />
             )}
             <button className="nav-button" onClick={handleToggleWeather}>
-              {showCurrentWeather ? 'Show Past Weather' : 'Show Current Weather'}
+              {showCurrentWeather ? "Show Past Weather" : "Show Current Weather"}
             </button>
           </div>
         </div>
@@ -99,10 +107,11 @@ const ImageGallery = () => {
             width="100%"
             height="400"
             src={`https://maps.google.com/maps?q=${images[currentImageIndex]?.latitude},${images[currentImageIndex]?.longitude}&output=embed`}
-          >
-          </iframe>
+          ></iframe>
         )}
-        <br /><br /><br />
+        <br />
+        <br />
+        <br />
       </div>
     </div>
   );
